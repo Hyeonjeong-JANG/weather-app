7787e1c9878ab8a2ac981fab6a9cabab8e17e7bf
getinthere <getinthere@naver.com>
2024-06-07 17:53:15 +0900

    공공데이터 날씨앱 완료

---
+++ src/main/java/shop/mtcoding/weather/_core/db/DBConnection.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.weather._core.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection getInstance(){
+        String username = "root";
+        String password = "root1234";
+        String url = "jdbc:mysql://localhost:3306/weatherdb";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
